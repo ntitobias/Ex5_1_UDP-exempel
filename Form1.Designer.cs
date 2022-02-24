3a3ca9fd99885c85df6823ae324c351f6c75a17e
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2022-02-24 23:33:02 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Ex5_1_UDP_klient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxMeddelande = new System.Windows.Forms.TextBox();
+            this.btnSkicka = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxMeddelande
+            // 
+            this.tbxMeddelande.Location = new System.Drawing.Point(30, 112);
+            this.tbxMeddelande.Multiline = true;
+            this.tbxMeddelande.Name = "tbxMeddelande";
+            this.tbxMeddelande.Size = new System.Drawing.Size(278, 107);
+            this.tbxMeddelande.TabIndex = 0;
+            // 
+            // btnSkicka
+            // 
+            this.btnSkicka.Location = new System.Drawing.Point(30, 235);
+            this.btnSkicka.Name = "btnSkicka";
+            this.btnSkicka.Size = new System.Drawing.Size(278, 29);
+            this.btnSkicka.TabIndex = 1;
+            this.btnSkicka.Text = "Sänd";
+            this.btnSkicka.UseVisualStyleBackColor = true;
+            this.btnSkicka.Click += new System.EventHandler(this.btnSkicka_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Utkorg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 294);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSkicka);
+            this.Controls.Add(this.tbxMeddelande);
+            this.Name = "Form1";
+            this.Text = "UDP klient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxMeddelande;
+        private System.Windows.Forms.Button btnSkicka;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex5_1_UDP_klient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSkicka_Click(object sender, EventArgs e)
+        {
+            //Skicka ett meddealnde till en server
+
+            //Deklarera vart vi ska skicka meddelandet
+            IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+            IPEndPoint destination = new IPEndPoint(serverIP, 12345);
+
+            //Omvandla meddelandet till något som går att skicka
+            byte[] message = Encoding.Unicode.GetBytes(tbxMeddelande.Text);
+
+            //Skapa en klient
+            UdpClient klient = new UdpClient();
+
+            //Skicka meddelandet
+            klient.Send(message, message.Length, destination);
+        }
+    }
+}
